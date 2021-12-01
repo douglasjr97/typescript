f78b1b5c9ef094b96732da009352a93b78f17b92
Douglas Falcao <falcao.douglas97@gmail.com>
2021-11-30 23:02:40 -0300

    type

---
+++ ES6/nameEspaces.ts
@@ -0,0 +1,11 @@
+namespace teste {
+  export function terra(base: number){
+       return base
+   }
+
+   export function marte(base2: number){
+    return base2
+}
+}
+
+console.log(teste.terra(6)-teste.marte(3)) 
